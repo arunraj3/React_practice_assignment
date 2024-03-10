7e1bcd52412f9886aad6ac21b7bebe93ed3609ae
arunraj3 <arunrajgovindan2002@gmail.com>
2024-03-10 20:34:49 +0530

    update App.js

--- assignment_one/App.js
+++ assignment_one/App.js
@@ -1,4 +1,23 @@
-const heading = React.createElement("h1",{},"Hello world using React");
+// const heading = React.createElement("h1",{},"Hello world using React");
 
-console.log(heading)
-ReactDOM.createRoot(document.getElementById("root")).render(heading);
+// console.log(heading)
+// ReactDOM.createRoot(document.getElementById("root")).render(heading);
+
+{
+  /* <div id="parent">
+        <div id="child">
+          <h1>This is H1 Tag within child</h1>
+        </div>
+      </div> */
+}
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [React.createElement("h1", {}, "This is H1 Tag within child"),React.createElement("h2", {}, "This is H2 Tag within child")])))
+
